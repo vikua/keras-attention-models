96039e5d6741ab6bda50378147ea939e06633c99
Viktor Kovryzhkin <vik.kovrizhkin@gmail.com>
2019-09-29 15:36:42 +0300

    initial

---
+++ Makefile
@@ -0,0 +1,50 @@
+FLAGS=
+
+
+black:
+	black keras-attention-models tests setup.py
+
+flake:
+	flake8 keras-attention-models tests setup.py
+
+
+test: flake
+	py.test -s -v $(FLAGS) ./tests/
+
+
+mypy:
+	mypy keras-attention-models --ignore-missing-imports --disallow-untyped-calls --no-site-packages --strict
+
+
+cov cover coverage: flake
+	py.test -s -v --cov-report term --cov-report html --cov keras-attention-models ./tests
+	@echo "open file://`pwd`/htmlcov/index.html"
+
+
+cov_only: flake
+	py.test -s -v --cov-report term --cov-report html --cov keras-attention-models ./tests
+	@echo "open file://`pwd`/htmlcov/index.html"
+
+
+clean:
+	rm -rf `find . -name __pycache__`
+	rm -f `find . -type f -name '*.py[co]' `
+	rm -f `find . -type f -name '*~' `
+	rm -f `find . -type f -name '.*~' `
+	rm -f `find . -type f -name '@*' `
+	rm -f `find . -type f -name '#*#' `
+	rm -f `find . -type f -name '*.orig' `
+	rm -f `find . -type f -name '*.rej' `
+	rm -f .coverage
+	rm -rf coverage
+	rm -rf build
+	rm -rf htmlcov
+	rm -rf dist
+
+
+doc:
+	make -C docs html
+	@echo "open file://`pwd`/docs/_build/html/index.html"
+
+
+.PHONY: all flake test cov clean doc
---
+++ setup.py
@@ -0,0 +1,70 @@
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+PY_VER = sys.version_info
+
+if not PY_VER >= (3, 6):
+    raise RuntimeError("Sorry, only Python 3.6 and later")
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+
+install_requires = ["Keras==2.3.0", "tensorflow==1.14.0"]
+extras_require = {}
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__), "mung", "__init__.py")
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            msg = "Cannot find version in mung/__init__.py"
+            raise RuntimeError(msg)
+
+
+classifiers = [
+    "License :: OSI Approved :: Apache Software License",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Operating System :: POSIX",
+    "Development Status :: 2 - Pre-Alpha",
+]
+
+
+setup(
+    name="keras-attention-models",
+    version=read_version(),
+description=("keras attention models"),
+    long_description=read("README.md"),
+    install_requires=install_requires,
+    classifiers=classifiers,
+    platforms=["POSIX"],
+    author="Viktor Kovryzhkin",
+    author_email="vik.kovrizhkin@gmail.com",
+    url="https://github.com/vikua/keras-attention-models",
+    download_url="",
+    license="Apache 2",
+    packages=find_packages(),
+    extras_require=extras_require,
+    keywords=[
+        "machine learning",
+        "deep learning",
+        "seq2seq",
+        "attention",
+        "luong",
+        "bahdanau",
+    ],
+    zip_safe=True,
+    include_package_data=True,
+)
